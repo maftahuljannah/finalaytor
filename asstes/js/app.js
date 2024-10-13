48e23568dff0e005ff480d97bc3b6a73691cfe88
maftahuljannah <maftahuljannah@gmail.com>
2024-10-13 16:26:37 +0600

    v-2

--- asstes/js/app.js
+++ asstes/js/app.js
@@ -184,7 +184,27 @@ $(function(){
     autoplay: true,            
     autoplaySpeed: 2000,       
     slidesToShow: 4,         
-    slidesToScroll: 4          
+    slidesToScroll: 4, 
+    responsive: [
+      {
+        breakpoint: 780,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+        
   });
           
 })
